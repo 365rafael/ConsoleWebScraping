6dbd806e235be192b5fe3153e09ff54722f903e8
Rafael Arantes <rafael-fullstack@outlook.com>
2022-12-23 11:16:14 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleWebScraping/DTOs/IbovespaInfoDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleWebScraping.DTOs
+{
+    public class IbovespaInfoDTO
+    {
+        public IbovespaInfoDTO()
+        {
+        }
+
+        public IbovespaInfoDTO(string totalPoints, string percentageVariation)
+        {
+            TotalPoints = totalPoints;
+            PercentageVariation = percentageVariation;
+        }
+
+        public string TotalPoints { get; set; }
+
+        public string PercentageVariation { get; set; }
+    }
+}
---
+++ ConsoleWebScraping/IbovespaWebScraping.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleWebScraping.DTOs;
+using HtmlAgilityPack;
+
+namespace ConsoleWebScraping
+{
+    public class IbovespaWebScraping
+    {
+        public IbovespaInfoDTO GetIbovespaInfo()
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load("https://www.infomoney.com.br/cotacoes/ibovespa/");
+
+            var ibovespaPointsNode = doc.DocumentNode.SelectNodes("//div[@class='value']/p");
+            var ibovespaPercentageNode = doc.DocumentNode.SelectNodes("//div[@class='percentage']/p");
+            var ibovespaTotalPoints = ibovespaPointsNode[0].InnerText.Trim();//trim remove espaços
+            var ibovespaPercentageVariation = ibovespaPercentageNode[0].InnerText.Trim();
+            return new IbovespaInfoDTO(ibovespaTotalPoints, ibovespaPercentageVariation);
+        }
+    }
+}
